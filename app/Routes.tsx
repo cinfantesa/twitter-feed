0e70f4646a3011b731f6ccfb6d4af23f0746e6d7
Cristobal Infantes <cinfantes@grupoasv.com>
2020-10-02 15:01:22 +0200

    removing innecessary configuration

--- app/Routes.tsx
+++ app/Routes.tsx
@@ -3,25 +3,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import routes from './constants/routes.json';
 import App from './containers/App';
-import HomePage from './containers/HomePage';
-
-// Lazily load routes and code split with webpack
-const LazyCounterPage = React.lazy(() =>
-  import(/* webpackChunkName: "CounterPage" */ './containers/CounterPage')
-);
-
-const CounterPage = (props: Record<string, any>) => (
-  <React.Suspense fallback={<h1>Loading...</h1>}>
-    <LazyCounterPage {...props} />
-  </React.Suspense>
-);
+import Feed from './components/feed/Feed';
 
 export default function Routes() {
   return (
     <App>
       <Switch>
-        <Route path={routes.COUNTER} component={CounterPage} />
-        <Route path={routes.HOME} component={HomePage} />
+        <Route path={routes.HOME} component={Feed} />
       </Switch>
     </App>
   );
